b702c51440fdeb7a91bc2ef46a42a09ad6388e25
lwx <940914735@qq.com>
2022-06-21 11:15:41 +0800

    修改Header 不同角色添加礼物

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -14,14 +14,16 @@
         <template #reference>
           <div class="author">
             <i class="icon el-icon-s-custom" />
-            {{ userInfo && userInfo.nickName || '' }}
+            {{ (userInfo && userInfo.nickName) || "" }}
             <i class="el-icon-caret-bottom" />
           </div>
         </template>
         <div class="nickname">
-          <p>登录名：{{ userInfo && userInfo.name || '' }}</p>
-          <p>昵称：{{ userInfo && userInfo.name || '' }}</p>
-          <el-tag size="small" effect="dark" class="logout" @click="logout">退出</el-tag>
+          <p>登录名：{{ (userInfo && userN) || "" }}</p>
+          <p>昵称：{{ (userInfo && userN) || "" }}</p>
+          <el-tag size="small" effect="dark" class="logout" @click="logout"
+            >退出</el-tag
+          >
         </div>
       </el-popover>
     </div>
@@ -29,97 +31,111 @@
 </template>
 
 <script>
-import { onMounted, reactive, toRefs } from 'vue'
-import { useRouter } from 'vue-router'
-import axios from '@/utils/axios'
-import { localRemove, pathMap } from '@/utils'
+import { onMounted, reactive, toRefs } from "vue";
+import { useRouter } from "vue-router";
+import axios from "@/utils/axios";
+import { localRemove, pathMap } from "@/utils";
 export default {
-  name: 'Header',
+  name: "Header",
   setup() {
-    const router = useRouter()
+    const router = useRouter();
     const state = reactive({
-      name: 'dashboard',
+      name: "dashboard",
       userInfo: null,
-      hasBack: false
-    })
+      hasBack: false,
+      userN:''
+      
+    });
     onMounted(() => {
-      const pathname = window.location.hash.split('/')[1] || ''
-      if (!['login'].includes(pathname)) {
-        getUserInfo()
+      const pathname = window.location.hash.split("/")[1] || "";
+      if (!["login"].includes(pathname)) {
+        getUserInfo();
       }
-    })
+    });
     const getUserInfo = async () => {
-      const userInfo = await axios.get('/admin/profile')
-      state.userInfo = userInfo
-    }
+      const username = window.sessionStorage.getItem("username");
+      console.log(username);
+      if (username == "admin") {
+        const userInfo = await axios.get("/admin/profile");
+        state.userInfo = userInfo;
+        state.userN = userInfo.name;
+        //console.log(state.userN);
+      } else {
+        const userInfo = await axios.get("/admin/vendor/profile")
+        state.userInfo = userInfo;
+        state.userN = userInfo.nameEn;
+        //console.log(state.userN);
+      }
+    };
     const logout = () => {
-      axios.delete('/admin/logout').then(() => {
-        localRemove('token')
-        window.location.reload()
-      })
-    }
+      axios.delete("/admin/logout").then(() => {
+        localRemove("token");
+        window.location.reload();
+      });
+    };
     const back = () => {
-      router.back()
-    }
+      router.back();
+    };
     router.afterEach((to) => {
-      console.log('to', to)
-      const { id } = to.query
-      state.name = pathMap[to.name]
-      if (id && to.name == 'add') {
-        state.name = '编辑礼物'
+      console.log("to", to);
+      const { id } = to.query;
+      state.name = pathMap[to.name];
+      if (id && to.name == "add") {
+        state.name = "编辑礼物";
       }
-      if (id && to.name == 'addVendor') {
-        state.name = '编辑商家'
+      if (id && to.name == "addVendor") {
+        state.name = "编辑商家";
       }
-      state.hasBack = ['level2', 'level3', 'order_detail'].includes(to.name)
-    })
+      state.hasBack = ["level2", "level3", "order_detail"].includes(to.name);
+    });
     return {
       ...toRefs(state),
       logout,
-      back
-    }
-  }
-}
+      back,
+    };
+  },
+};
 </script>
 
 <style scoped>
-  .header {
-    height: 50px;
-    border-bottom: 1px solid #e9e9e9;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 0 20px;
-  }
-  .el-icon-back {
-    border: 1px solid #e9e9e9;
-    padding: 4px;
-    border-radius: 50px;
-    margin-right: 10px;
-  }
-  .right > div > .icon{
-    font-size: 18px;
-    margin-right: 6px;
-  }
-  .author {
-    margin-left: 10px;
-    cursor: pointer;
-  }
+.header {
+  height: 50px;
+  border-bottom: 1px solid #e9e9e9;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 20px;
+}
+.el-icon-back {
+  border: 1px solid #e9e9e9;
+  padding: 4px;
+  border-radius: 50px;
+  margin-right: 10px;
+}
+.right > div > .icon {
+  font-size: 18px;
+  margin-right: 6px;
+}
+.author {
+  margin-left: 10px;
+  cursor: pointer;
+}
 </style>
 <style>
-  .popper-user-box {
-    background: url('https://s.yezgea02.com/lingling-h5/static/account-banner-bg.png') 50% 50% no-repeat!important;
-    background-size: cover!important;
-    border-radius: 0!important;
-  }
-  .popper-user-box .nickname {
-    position: relative;
-    color: #ffffff;
-  }
-  .popper-user-box .nickname .logout {
-    position: absolute;
-    right: 0;
-    top: 0;
-    cursor: pointer;
-  }
+.popper-user-box {
+  background: url("https://s.yezgea02.com/lingling-h5/static/account-banner-bg.png")
+    50% 50% no-repeat !important;
+  background-size: cover !important;
+  border-radius: 0 !important;
+}
+.popper-user-box .nickname {
+  position: relative;
+  color: #ffffff;
+}
+.popper-user-box .nickname .logout {
+  position: absolute;
+  right: 0;
+  top: 0;
+  cursor: pointer;
+}
 </style>
--- src/views/AddGood.vue
+++ src/views/AddGood.vue
@@ -1,30 +1,73 @@
 <template>
   <div class="add">
     <el-card class="add-container">
-      <el-form :model="giftForm" :rules="rules" ref="giftRef" label-width="100px" class="giftForm">
+      <el-form
+        :model="giftForm"
+        :rules="rules"
+        ref="giftRef"
+        label-width="100px"
+        class="giftForm"
+      >
         <el-form-item required label="礼物分类">
-          <el-cascader :placeholder="defaultCate" style="width: 300px;" :props="category" @change="handleChangeCate"></el-cascader>
+          <el-cascader
+            :placeholder="defaultCate"
+            style="width: 300px"
+            :props="category"
+            @change="handleChangeCate"
+          ></el-cascader>
         </el-form-item>
         <el-form-item label="礼物名称" prop="giftName">
-          <el-input style="width: 300px" v-model="giftForm.giftName" placeholder="请输入礼物名称"></el-input>
+          <el-input
+            style="width: 300px"
+            v-model="giftForm.giftName"
+            placeholder="请输入礼物名称"
+          ></el-input>
         </el-form-item>
         <el-form-item label="礼物简介" prop="giftIntro">
-          <el-input style="width: 300px" type="textarea" v-model="giftForm.giftIntro" placeholder="请输入礼物简介(100字)"></el-input>
+          <el-input
+            style="width: 300px"
+            type="textarea"
+            v-model="giftForm.giftIntro"
+            placeholder="请输入礼物简介(100字)"
+          ></el-input>
         </el-form-item>
         <el-form-item label="礼物价格" prop="originalPrice">
-          <el-input type="number" min="0" style="width: 300px" v-model="giftForm.originalPrice" placeholder="请输入礼物价格"></el-input>
+          <el-input
+            type="number"
+            min="0"
+            style="width: 300px"
+            v-model="giftForm.originalPrice"
+            placeholder="请输入礼物价格"
+          ></el-input>
         </el-form-item>
         <el-form-item label="礼物会员价" prop="vipPrice">
-          <el-input type="number" min="0" style="width: 300px" v-model="giftForm.vipPrice" placeholder="请输入礼物售价"></el-input>
+          <el-input
+            type="number"
+            min="0"
+            style="width: 300px"
+            v-model="giftForm.vipPrice"
+            placeholder="请输入礼物售价"
+          ></el-input>
         </el-form-item>
         <el-form-item label="礼物库存" prop="stockNum">
-          <el-input type="number" min="0" style="width: 300px" v-model="giftForm.stockNum" placeholder="请输入礼物库存"></el-input>
+          <el-input
+            type="number"
+            min="0"
+            style="width: 300px"
+            v-model="giftForm.stockNum"
+            placeholder="请输入礼物库存"
+          ></el-input>
         </el-form-item>
         <!-- <el-form-item label="礼物标签" prop="giftLabelIdList">
           <el-input style="width: 300px" v-model="giftForm.giftLabelIdList" placeholder="请输入礼物小标签"></el-input>
         </el-form-item> -->
         <el-form-item required label="礼物标签">
-          <el-cascader :placeholder="defaultLabel" style="width: 300px;" :props="label" @change="handleChangeLabel"></el-cascader>
+          <el-cascader
+            :placeholder="defaultLabel"
+            style="width: 300px"
+            :props="label"
+            @change="handleChangeLabel"
+          ></el-cascader>
         </el-form-item>
         <el-form-item label="上架状态" prop="isShown">
           <el-radio-group v-model="giftForm.isShown">
@@ -38,21 +81,28 @@
             :action="uploadImgServer"
             accept="jpg,jpeg,png"
             :headers="{
-              token: token
+              token: token,
             }"
             :show-file-list="false"
             :before-upload="handleBeforeUpload"
             :on-success="handleUrlSuccess"
           >
-            <img style="width: 100px; height: 100px; border: 1px solid #e9e9e9;" v-if="giftForm.imgUrl" :src="giftForm.imgUrl" class="avatar">
+            <img
+              style="width: 100px; height: 100px; border: 1px solid #e9e9e9"
+              v-if="giftForm.imgUrl"
+              :src="giftForm.imgUrl"
+              class="avatar"
+            />
             <i v-else class="el-icon-plus avatar-uploader-icon"></i>
           </el-upload>
         </el-form-item>
         <el-form-item label="详情内容">
-          <div ref='editor'></div>
+          <div ref="editor"></div>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" @click="submitAdd()">{{ id ? '立即修改' : '立即创建' }}</el-button>
+          <el-button type="primary" @click="submitAdd()">{{
+            id ? "立即修改" : "立即创建"
+          }}</el-button>
         </el-form-item>
       </el-form>
     </el-card>
@@ -60,136 +110,148 @@
 </template>
 
 <script>
-import { reactive, ref, toRefs, onMounted, onBeforeUnmount, getCurrentInstance } from 'vue'
-import WangEditor from 'wangeditor'
-import axios from '@/utils/axios'
-import { ElMessage } from 'element-plus'
-import { useRoute, useRouter } from 'vue-router'
-import { localGet, uploadImgServer, uploadImgsServer, hasEmoji } from '@/utils'
+import {
+  reactive,
+  ref,
+  toRefs,
+  onMounted,
+  onBeforeUnmount,
+  getCurrentInstance,
+} from "vue";
+import WangEditor from "wangeditor";
+import axios from "@/utils/axios";
+import { ElMessage } from "element-plus";
+import { useRoute, useRouter } from "vue-router";
+import { localGet, uploadImgServer, uploadImgsServer, hasEmoji } from "@/utils";
 export default {
-  name: 'AddGood',
+  name: "AddGood",
   setup() {
-    const { proxy } = getCurrentInstance()
-    console.log('proxy', proxy)
-    const editor = ref(null)
-    const giftRef = ref(null)
-    const route = useRoute()
-    const router = useRouter()
-    const { id } = route.query
+    const { proxy } = getCurrentInstance();
+    console.log("proxy", proxy);
+    const editor = ref(null);
+    const giftRef = ref(null);
+    const route = useRoute();
+    const router = useRouter();
+    const { id } = route.query;
     const state = reactive({
       uploadImgServer,
-      token: localGet('token') || '',
+      token: localGet("token") || "",
       id: id,
-      defaultCate: '',
-      defaultLabel:'',
+      defaultCate: "",
+      defaultLabel: "",
       giftForm: {
-        giftName: '',
-        giftIntro: '',
-        originalPrice: '',
-        vipPrice: '',
-        stockNum: '',
-        isShown: '0',
-        imgUrl: '',
-        giftLabelIdList: ''
+        giftName: "",
+        giftIntro: "",
+        originalPrice: "",
+        vipPrice: "",
+        stockNum: "",
+        isShown: "0",
+        imgUrl: "",
+        giftLabelIdList: "",
       },
+      vendorId: "",
       rules: {
         // imgUrl: [
         //   { required: 'true', message: '请上传主图', trigger: ['change'] }
         // ],
         giftName: [
-          { required: 'true', message: '请填写礼物名称', trigger: ['change'] }
+          { required: "true", message: "请填写礼物名称", trigger: ["change"] },
         ],
         originalPrice: [
-          { required: 'true', message: '请填写礼物价格', trigger: ['change'] }
+          { required: "true", message: "请填写礼物价格", trigger: ["change"] },
         ],
         vipPrice: [
-          { required: 'true', message: '请填写礼物售价', trigger: ['change'] }
+          { required: "true", message: "请填写礼物售价", trigger: ["change"] },
         ],
         stockNum: [
-          { required: 'true', message: '请填写礼物库存', trigger: ['change'] }
+          { required: "true", message: "请填写礼物库存", trigger: ["change"] },
         ],
       },
-      categoryid: '',
+      categoryid: "",
       category: {
         lazy: true,
         lazyLoad(node, resolve) {
-          const { level = 0, value } = node
-          axios.get('/admin/categories/level', {
-            params: {
-              currentPage: 1,
-              pageSize: 10,
-              categoryLevel: level + 1,
-              parentId: value || 0
-            }
-          }).then(res => {
-            const list = res.list
-            const nodes = list.map(item => ({
-              value: item.categoryid,
-              label: item.categoryname,
-              leaf: level > 1
-            }))
-            resolve(nodes)
-          })
-        }
+          const { level = 0, value } = node;
+          axios
+            .get("/admin/categories/level", {
+              params: {
+                currentPage: 1,
+                pageSize: 10,
+                categoryLevel: level + 1,
+                parentId: value || 0,
+              },
+            })
+            .then((res) => {
+              const list = res.list;
+              const nodes = list.map((item) => ({
+                value: item.categoryid,
+                label: item.categoryname,
+                leaf: level > 1,
+              }));
+              resolve(nodes);
+            });
+        },
       },
-      labelid: '',
+      labelid: "",
       label: {
         lazy: true,
         lazyLoad(node, resolve) {
-          const { level = 0, value } = node
-          axios.get('/admin/label/labelLevel', {
-            params: {
-              currentPage: 1,
-              pageSize: 10,
-              labelLevel: level + 1,
-              parentId: value || 0
-            }
-          }).then(res => {
-            const list = res.list
-            const nodes = list.map(item => ({
-              value: item.labelid,
-              label: item.labelname,
-              leaf: level > 1
-            }))
-            resolve(nodes)
-          })
-        }
-      }
-    })
-    let instance
+          const { level = 0, value } = node;
+          axios
+            .get("/admin/label/labelLevel", {
+              params: {
+                currentPage: 1,
+                pageSize: 10,
+                labelLevel: level + 1,
+                parentId: value || 0,
+              },
+            })
+            .then((res) => {
+              const list = res.list;
+              const nodes = list.map((item) => ({
+                value: item.labelid,
+                label: item.labelname,
+                leaf: level > 1,
+              }));
+              resolve(nodes);
+            });
+        },
+      },
+    });
+    let instance;
     onMounted(() => {
-      instance = new WangEditor(editor.value)
-      instance.config.showLinkImg = false
-      instance.config.showLinkImgAlt = false
-      instance.config.showLinkImgHref = false
-      instance.config.uploadImgMaxSize = 2 * 1024 * 1024 // 2M
-      instance.config.uploadFileName = 'file'
+      instance = new WangEditor(editor.value);
+      instance.config.showLinkImg = false;
+      instance.config.showLinkImgAlt = false;
+      instance.config.showLinkImgHref = false;
+      instance.config.uploadImgMaxSize = 2 * 1024 * 1024; // 2M
+      instance.config.uploadFileName = "file";
       instance.config.uploadImgHeaders = {
-        token: state.token
-      }
+        token: state.token,
+      };
       // 图片返回格式不同，需要自定义返回格式
       instance.config.uploadImgHooks = {
         // 图片上传并返回了结果，想要自己把图片插入到编辑器中
         // 例如服务器端返回的不是 { errno: 0, data: [...] } 这种格式，可使用 customInsert
-        customInsert: function(insertImgFn, result) {
-          console.log('result', result)
+        customInsert: function (insertImgFn, result) {
+          console.log("result", result);
           // result 即服务端返回的接口
           // insertImgFn 可把图片插入到编辑器，传入图片 src ，执行函数即可
           if (result.data && result.data.length) {
-            result.data.forEach(item => insertImgFn(item))
+            result.data.forEach((item) => insertImgFn(item));
           }
-        }
-      }
-      instance.config.uploadImgServer = uploadImgsServer
+        },
+      };
+      instance.config.uploadImgServer = uploadImgsServer;
       Object.assign(instance.config, {
         onchange() {
-          console.log('change')
+          console.log("change");
         },
-      })
-      instance.create()
+      });
+      instance.create();
       if (id) {
-        axios.get(`/admin/gift/${id}`).then(res => {
-          const { gift, categories } = res
+        axios.get(`/admin/gift/${id}`).then((res) => {
+          const { gift, categories } = res;
           state.giftForm = {
             giftName: gift.giftName,
             // giftIntro: gift.giftIntro,
@@ -199,26 +261,40 @@ export default {
             // isShown: String(gift.isShown),
             // imgUrl: proxy.$filters.prefix(gift.imgUrl),
             // giftLabelIdList: gift.giftLabelIdList,
-             categoryid: categories.categoryid
-          }
+            categoryid: categories.categoryid,
+          };
           //state.categoryid = categories.categoryid
           //state.defaultCate = `${firstCategory.categoryName}/${secondCategory.categoryName}/${thirdCategory.categoryName}`
           if (instance) {
             // 初始化礼物详情 html
-            instance.txt.html(gift.giftDetail)
+            instance.txt.html(gift.giftDetail);
           }
-        })
+        });
       }
-    })
+
+      const username = window.sessionStorage.getItem("username");
+      //console.log(username);
+      if (username == "admin") {
+        axios.get("/admin/profile").then((res) => {
+          state.vendorId = res.adminID;
+          //console.log(state.vendorId);
+        });
+      } else {
+        axios.get("/admin/vendor/profile").then((res) => {
+          state.vendorId = res.vendorId;
+          //console.log(state.vendorId);
+        });
+      }
+    });
     onBeforeUnmount(() => {
-      instance.destroy()
-      instance = null
-    })
+      instance.destroy();
+      instance = null;
+    });
     const submitAdd = () => {
       giftRef.value.validate((vaild) => {
         if (vaild) {
           // 默认新增用 post 方法
-          let httpOption = axios.post
+          let httpOption = axios.post;
           let params = {
             giftId: state.giftForm.giftId,
             giftName: state.giftForm.giftName,
@@ -231,56 +307,59 @@ export default {
             isShown: state.giftForm.isShown,
             imgUrl: state.giftForm.imgUrl,
             giftDetail: instance.txt.html(),
-            vendorId: 11
-            
-            
-          }
-          if (hasEmoji(params.giftIntro) || hasEmoji(params.giftName) || hasEmoji(params.giftLabelIdList) || hasEmoji(params.giftDetail)) {
-            ElMessage.error('不要输入表情包，再输入就打死你个龟孙儿~')
-            return
+            vendorId: state.vendorId,
+          };
+          if (
+            hasEmoji(params.giftIntro) ||
+            hasEmoji(params.giftName) ||
+            hasEmoji(params.giftLabelIdList) ||
+            hasEmoji(params.giftDetail)
+          ) {
+            ElMessage.error("不要输入表情包，再输入就打死你个龟孙儿~");
+            return;
           }
           if (params.giftName.length > 30) {
-            ElMessage.error('礼物名称不能超过30个字符')
-            return
+            ElMessage.error("礼物名称不能超过30个字符");
+            return;
           }
           if (params.giftIntro.length > 100) {
-            ElMessage.error('礼物简介不能超过100个字符')
-            return
+            ElMessage.error("礼物简介不能超过100个字符");
+            return;
           }
           if (params.giftLabelIdList.length > 16) {
-            ElMessage.error('礼物标签不能超过16个字符')
-            return
+            ElMessage.error("礼物标签不能超过16个字符");
+            return;
           }
-          console.log('params', params)
+          console.log("params", params);
           if (id) {
-            params.giftId = id
+            params.giftId = id;
             // 修改礼物使用 put 方法
-            httpOption = axios.put
+            httpOption = axios.put;
           }
-          httpOption('/admin/gift', params).then(() => {
-            ElMessage.success(id ? '修改成功' : '添加成功')
-            router.push({ path: '/good' })
-          })
+          httpOption("/admin/gift", params).then(() => {
+            ElMessage.success(id ? "修改成功" : "添加成功");
+            router.push({ path: "/good" });
+          });
         }
-      })
-    }
+      });
+    };
     const handleBeforeUpload = (file) => {
-      const sufix = file.name.split('.')[1] || ''
-      if (!['jpg', 'jpeg', 'png'].includes(sufix)) {
-        ElMessage.error('请上传 jpg、jpeg、png 格式的图片')
-        return false
+      const sufix = file.name.split(".")[1] || "";
+      if (!["jpg", "jpeg", "png"].includes(sufix)) {
+        ElMessage.error("请上传 jpg、jpeg、png 格式的图片");
+        return false;
       }
-    }
+    };
     const handleUrlSuccess = (val) => {
-      state.giftForm.imgUrl = val.data || ''
-    }
+      state.giftForm.imgUrl = val.data || "";
+    };
     const handleChangeCate = (val) => {
-      state.categoryid = val[2] || 0
-    }
+      state.categoryid = val[2] || 0;
+    };
 
     const handleChangeLable = (val) => {
-      state.giftLabelIdList = val[2] || 0
-    }
+      state.giftLabelIdList = val[2] || 0;
+    };
     return {
       ...toRefs(state),
       giftRef,
@@ -289,31 +368,31 @@ export default {
       handleUrlSuccess,
       editor,
       handleChangeCate,
-      handleChangeLable
-    }
-  }
-}
+      handleChangeLable,
+    };
+  },
+};
 </script>
 
 <style scoped>
-  .add {
-    display: flex;
-  }
-  .add-container {
-    flex: 1;
-    height: 100%;
-  }
-  .avatar-uploader {
-    width: 100px;
-    height: 100px;
-    color: #ddd;
-    font-size: 30px;
-  }
-  .avatar-uploader-icon {
-    display: block;
-    width: 100%;
-    height: 100%;
-    border: 1px solid #e9e9e9;
-    padding: 32px 17px;
-  }
+.add {
+  display: flex;
+}
+.add-container {
+  flex: 1;
+  height: 100%;
+}
+.avatar-uploader {
+  width: 100px;
+  height: 100px;
+  color: #ddd;
+  font-size: 30px;
+}
+.avatar-uploader-icon {
+  display: block;
+  width: 100%;
+  height: 100%;
+  border: 1px solid #e9e9e9;
+  padding: 32px 17px;
+}
 </style>
