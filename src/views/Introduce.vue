a8e77c519db18110c613bfa112898cdcaa502be1
lwx <940914735@qq.com>
2022-07-24 21:18:08 +0800

    小修改

--- src/views/Introduce.vue
+++ src/views/Introduce.vue
@@ -2,8 +2,9 @@
   <el-card class="account-container">
 
     <ul>
-      <li></li>
-      <li></li>
+      <li>为Heatbeat365服务的管理系统</li>
+      <li>为商家提供所属礼物，订单，定制服务管理</li>
+      <li>为管理员提供首页，分类，标签，礼物，订单，商家，定制服务，优惠券，活动管理</li>
     </ul>
   </el-card>
 </template>
--- src/views/Order.vue
+++ src/views/Order.vue
@@ -10,7 +10,12 @@
           size="small"
           clearable
         />
-        <el-select @change="handleOption" v-model="orderStatus" size="small" style="width: 200px; margin-right: 10px">
+        <el-select
+          @change="handleOption"
+          v-model="orderStatus"
+          size="small"
+          style="width: 200px; margin-right: 10px"
+        >
           <el-option
             v-for="item in options"
             :key="item.value"
@@ -19,10 +24,34 @@
           />
         </el-select>
         <!-- <el-button type="primary" size="small" icon="el-icon-edit">修改订单</el-button> -->
-        <el-button type="primary" size="small" icon="el-icon-s-home" @click="handleStart()">开始定制</el-button>
-        <el-button type="primary" size="small" icon="el-icon-s-home" @click="handleConfig()">定制完成</el-button>
-        <el-button type="primary" size="small" icon="el-icon-s-home" @click="handleSend()">出库</el-button>
-        <el-button type="danger" size="small" icon="el-icon-delete" @click="handleClose()">关闭订单</el-button>
+        <el-button
+          type="primary"
+          size="small"
+          icon="el-icon-s-home"
+          @click="handleStart()"
+          >开始定制</el-button
+        >
+        <el-button
+          type="primary"
+          size="small"
+          icon="el-icon-s-home"
+          @click="handleConfig()"
+          >定制完成</el-button
+        >
+        <el-button
+          type="primary"
+          size="small"
+          icon="el-icon-s-home"
+          @click="handleSend()"
+          >出库</el-button
+        >
+        <el-button
+          type="danger"
+          size="small"
+          icon="el-icon-delete"
+          @click="handleClose()"
+          >关闭订单</el-button
+        >
       </div>
     </template>
     <el-table
@@ -31,51 +60,33 @@
       tooltip-effect="dark"
       style="width: 100%"
       @selection-change="handleSelectionChange"
-      align="center">
-      <el-table-column
-        type="selection"
-        width="55">
-      </el-table-column>
-      <el-table-column
-        prop="orderNo"
-        label="订单号"
-      >
-      </el-table-column>
-      <el-table-column
-        prop="totalPrice"
-        label="订单总价"
-        align="center"
-      >
-      <template #default="scope">
+      align="center"
+    >
+      <el-table-column type="selection" width="55"> </el-table-column>
+      <el-table-column prop="orderNo" label="订单号"> </el-table-column>
+      <el-table-column prop="totalPrice" label="订单总价" align="center">
+        <template #default="scope">
           <span>¥ {{ scope.row.totalPrice }}</span>
         </template>
       </el-table-column>
-      <el-table-column
-        prop="orderStatus"
-        label="订单状态"
-      >
+      <el-table-column prop="orderStatus" label="订单状态">
         <template #default="scope">
           <span>{{ $filters.orderMap(scope.row.orderStatus) }}</span>
         </template>
       </el-table-column>
-      <el-table-column
-        prop="payType"
-        label="支付方式"
-      >
-        <template #default='scope'>
+      <el-table-column prop="payType" label="支付方式">
+        <template #default="scope">
           <span v-if="scope.row.payType == 1">微信支付</span>
           <span v-else-if="scope.row.payType == 2">支付宝支付</span>
           <span v-else>未支付</span>
         </template>
       </el-table-column>
-      <el-table-column
-        prop="createTime"
-        label="创建时间"
-      >
+      <el-table-column prop="createTime" label="创建时间">
+        <template #default="scope">
+          <span>{{ formatDate(scope.row.createTime) }}</span>
+        </template>
       </el-table-column>
-      <el-table-column
-        label="操作"
-      >
+      <el-table-column label="操作">
         <template #default="scope">
           <el-popconfirm
             v-if="scope.row.orderStatus == 1"
@@ -113,7 +124,10 @@
               <a style="cursor: pointer; margin-right: 10px">关闭订单</a>
             </template>
           </el-popconfirm>
-          <router-link :to="{ path: '/order_detail', query: { id: scope.row.orderNo }}">订单详情</router-link>
+          <router-link
+            :to="{ path: '/order_detail', query: { id: scope.row.orderNo } }"
+            >订单详情</router-link
+          >
         </template>
       </el-table-column>
     </el-table>
@@ -130,11 +144,11 @@
 </template>
 
 <script>
-import { onMounted, reactive, ref, toRefs } from 'vue'
-import { ElMessage } from 'element-plus'
-import axios from '@/utils/axios'
+import { onMounted, reactive, ref, toRefs } from "vue";
+import { ElMessage } from "element-plus";
+import axios from "@/utils/axios";
 export default {
-  name: 'Order',
+  name: "Order",
   setup() {
     const state = reactive({
       loading: false,
@@ -143,146 +157,187 @@ export default {
       total: 0, // 总条数
       currentPage: 1, // 当前页
       pageSize: 10, // 分页大小
-      orderNo: '', // 订单号
-      orderStatus: '', // 订单状态
-      options: [{
-        value: '',
-        label: '全部'
-      }, {
-        value: 0,
-        label: '待支付'
-      }, {
-        value: 1,
-        label: '已支付'
-      }, {
-        value: 2,
-        label: '定制中'
-      }, {
-        value: 3,
-        label: '定制完成'
-      }, {
-        value: 4,
-        label: '出库成功'
-      }, {
-        value: 5,
-        label: '交易成功'
-      }, {
-        value: 6,
-        label: '手动关闭'
-      }, {
-        value: 7,
-        label: '用户取消'
-      }]
-    })
+      orderNo: "", // 订单号
+      orderStatus: "", // 订单状态
+      options: [
+        {
+          value: "",
+          label: "全部",
+        },
+        {
+          value: 0,
+          label: "待支付",
+        },
+        {
+          value: 1,
+          label: "已支付",
+        },
+        {
+          value: 2,
+          label: "定制中",
+        },
+        {
+          value: 3,
+          label: "定制完成",
+        },
+        {
+          value: 4,
+          label: "出库成功",
+        },
+        {
+          value: 5,
+          label: "交易成功",
+        },
+        {
+          value: 6,
+          label: "手动关闭",
+        },
+        {
+          value: 7,
+          label: "用户取消",
+        },
+      ],
+    });
     onMounted(() => {
-      getOrderList()
-    })
+      getOrderList();
+    });
     // 获取轮播图列表
     const getOrderList = () => {
-      state.loading = true
-      axios.get('/admin/order/list', {
-        params: {
-          pageNum: state.currentPage,
-          pageSize: state.pageSize,
-          orderNo: state.orderNo,
-          orderStatus: state.orderStatus
-        }
-      }).then(res => {
-        state.tableData = res.list
-        state.total = res.totalCount
-        state.currentPage = res.currPage
-        state.loading = false
-      })
-    }
+      state.loading = true;
+      axios
+        .get("/admin/order/list", {
+          params: {
+            pageNum: state.currentPage,
+            pageSize: state.pageSize,
+            orderNo: state.orderNo,
+            orderStatus: state.orderStatus,
+          },
+        })
+        .then((res) => {
+          state.tableData = res.list;
+          state.total = res.totalCount;
+          state.currentPage = res.currPage;
+          state.loading = false;
+        });
+    };
     const handleOption = () => {
-      state.currentPage = 1
-      getOrderList()
-    }
+      state.currentPage = 1;
+      getOrderList();
+    };
     // 选择项
     const handleSelectionChange = (val) => {
-      state.multipleSelection = val
+      state.multipleSelection = val;
       //console.log(val)
-    }
+    };
     const changePage = (val) => {
-      state.currentPage = val
-      getOrderList()
-    }
+      state.currentPage = val;
+      getOrderList();
+    };
     const handleStart = (id) => {
-      console.log('id', id)
-      let params
+      console.log("id", id);
+      let params;
       if (id) {
-        params = [id].join(",")
+        params = [id].join(",");
       } else {
         if (!state.multipleSelection.length) {
-          console.log('state.multipleSelection', state.multipleSelection.length)
-          ElMessage.error('请选择项')
-          return
+          console.log(
+            "state.multipleSelection",
+            state.multipleSelection.length
+          );
+          ElMessage.error("请选择项");
+          return;
         }
-        params = state.multipleSelection.map(i => i.orderNo).join(",")
+        params = state.multipleSelection.map((i) => i.orderNo).join(",");
       }
-      axios.put('/admin/order/startCustomService', {
-        orderNos: params
-      }).then(() => {
-        ElMessage.success('修改成功')
-        getOrderList()
-      })
-    }
+      axios
+        .put("/admin/order/startCustomService", {
+          orderNos: params,
+        })
+        .then(() => {
+          ElMessage.success("修改成功");
+          getOrderList();
+        });
+    };
     const handleConfig = (id) => {
-      console.log('id', id)
-      let params
+      console.log("id", id);
+      let params;
       if (id) {
-        params = [id].join(",")
+        params = [id].join(",");
       } else {
         if (!state.multipleSelection.length) {
-          console.log('state.multipleSelection', state.multipleSelection.length)
-          ElMessage.error('请选择项')
-          return
+          console.log(
+            "state.multipleSelection",
+            state.multipleSelection.length
+          );
+          ElMessage.error("请选择项");
+          return;
         }
-        params = state.multipleSelection.map(i => i.orderNo).join(",")
+        params = state.multipleSelection.map((i) => i.orderNo).join(",");
       }
-      axios.put('/admin/order/endCustomService', {
-        orderNos: params
-      }).then(() => {
-        ElMessage.success('修改成功')
-        getOrderList()
-      })
-    }
+      axios
+        .put("/admin/order/endCustomService", {
+          orderNos: params,
+        })
+        .then(() => {
+          ElMessage.success("修改成功");
+          getOrderList();
+        });
+    };
     const handleSend = (id) => {
-      let params
+      let params;
       if (id) {
-        params = [id].join(",")
+        params = [id].join(",");
       } else {
         if (!state.multipleSelection.length) {
-          ElMessage.error('请选择项')
-          return
+          ElMessage.error("请选择项");
+          return;
         }
-        params = state.multipleSelection.map(i => i.orderNo).join(",")
+        params = state.multipleSelection.map((i) => i.orderNo).join(",");
       }
-      axios.put('/admin/order/checkout', {
-        orderNos: params
-      }).then(() => {
-        ElMessage.success('出库成功')
-        getOrderList()
-      })
-    }
+      axios
+        .put("/admin/order/checkout", {
+          orderNos: params,
+        })
+        .then(() => {
+          ElMessage.success("出库成功");
+          getOrderList();
+        });
+    };
     const handleClose = (id) => {
-      let params
+      let params;
       if (id) {
-        params = [id].join(",")
+        params = [id].join(",");
       } else {
         if (!state.multipleSelection.length) {
-          ElMessage.error('请选择项')
-          return
+          ElMessage.error("请选择项");
+          return;
         }
-        params = state.multipleSelection.map(i => i.orderNo).join(",")
+        params = state.multipleSelection.map((i) => i.orderNo).join(",");
       }
-      axios.put('/admin/order/close', {
-        orderNos: params
-      }).then(() => {
-        ElMessage.success('关闭成功')
-        getOrderList()
-      })
-    }
+      axios
+        .put("/admin/order/close", {
+          orderNos: params,
+        })
+        .then(() => {
+          ElMessage.success("关闭成功");
+          getOrderList();
+        });
+    };
+    const formatDate = (value) => {
+      let date = new Date(value);
+      let y = date.getFullYear();
+      let MM = date.getMonth() + 1;
+      MM = MM < 10 ? "0" + MM : MM;
+      let d = date.getDate();
+      d = d < 10 ? "0" + d : d;
+      let h = date.getHours();
+      h = h < 10 ? "0" + h : h;
+      let m = date.getMinutes();
+      m = m < 10 ? "0" + m : m;
+      let s = date.getSeconds();
+      s = s < 10 ? "0" + s : s;
+      return y + "-" + MM + "-" + d + " " + h + ":" + m + ":" + s;
+    };
     return {
       ...toRefs(state),
       handleSelectionChange,
@@ -292,17 +347,18 @@ export default {
       handleStart,
       handleConfig,
       handleSend,
-      handleClose
-    }
-  }
-}
+      handleClose,
+      formatDate
+    };
+  },
+};
 </script>
 
 <style scoped>
-  .order-container {
-    min-height: 100%;
-  }
-  .el-card.is-always-shadow {
-    min-height: 100%!important;
-  }
+.order-container {
+  min-height: 100%;
+}
+.el-card.is-always-shadow {
+  min-height: 100% !important;
+}
 </style>
